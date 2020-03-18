146394604a8a3a85e8424da1650edc0dc0decd08
TrushkinaVera <50058579+TrushkinaVera@users.noreply.github.com>
2020-03-18 23:18:12 +0300

    Add files via upload

---
+++ src/main/java/main/java/model/Calculator.java
@@ -0,0 +1,17 @@
+package main.java.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+
+    private boolean isInArea(double x, double y, double r) {
+        if (x>=0) return y>=0&&Math.hypot(x,y)<=r||(x==0&&Math.abs(y)<=r);
+        if (y>0) return Math.abs(2*y)<=r&&Math.abs(x)<=r;
+        return Math.abs(x)+Math.abs(y)<=r;
+    }
+
+    public boolean isInArea(Point point) {
+        return isInArea(point.getX(), point.getY(), point.getR());
+    }
+}
